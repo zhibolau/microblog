47a4a6031283e2873267b794e6a54a81c2b3670f
Zhibo Liu <ZhiboLIU@Zhibos-MacBook-Pro.local>
2019-07-07 20:26:46 -0700

    added about-me last_seen in user;also db updated

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
@@ -76,3 +78,8 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return  render_template('user.html', user=user, posts =posts)
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
